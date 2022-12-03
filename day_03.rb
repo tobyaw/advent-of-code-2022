705053bdd1a3202c13ecde430c78724150e86804
Toby Atkin-Wright <tobyaw@me.com>
2022-12-03 13:26:25 +0000

    Turning the input into character arrays on load. Compacting other operations.

--- day_03.rb
+++ day_03.rb
@@ -1,12 +1,6 @@
 #!/usr/bin/env ruby
 
-priorities = (('a'..'z').to_a + ('A'..'Z').to_a).zip((1..52)).to_h
-input = File.read('day_03_input.txt').split("\n")
-
-puts input.map { |i| i.scan(/.{#{i.size / 2}}/) }
-          .map { |i| priorities[(i[0].chars & i[1].chars).first] }
-          .sum
-
-puts input.each_slice(3)
-          .map { |i| priorities[(i[0].chars & i[1].chars & i[2].chars).first] }
-          .sum
+pri = (('a'..'z').to_a + ('A'..'Z').to_a).zip((1..52)).to_h
+inp = File.read('day_03_input.txt').split("\n").map(&:chars)
+puts inp.map { |i| i.each_slice(i.size / 2).to_a }.map { |i| pri[(i[0] & i[1]).first] }.sum
+puts inp.each_slice(3).map { |i| pri[(i[0] & i[1] & i[2]).first] }.sum
