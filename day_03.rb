6e98d731d7e2cabc2fe51e3f7ac4c47a47f3aea6
Toby Atkin-Wright <tobyaw@me.com>
2022-12-03 13:52:44 +0000

    Aha, makes much more sense to use a reduce or two.

--- day_03.rb
+++ day_03.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 
-pri = (('a'..'z').to_a + ('A'..'Z').to_a).zip((1..52)).to_h
-inp = File.read('day_03_input.txt').split("\n").map(&:chars)
-puts inp.map { |i| i.each_slice(i.size / 2).to_a }.map { |i| pri[(i[0] & i[1]).first] }.sum
-puts inp.each_slice(3).map { |i| pri[(i[0] & i[1] & i[2]).first] }.sum
+PRIORITY = [*('a'..'z'), *('A'..'Z')].zip((1..52)).to_h
+input = File.read('day_03_input.txt').split("\n").map(&:chars)
+puts input.map { |i| PRIORITY[i.each_slice(i.size / 2).reduce(:&).first] }.sum
+puts input.each_slice(3).map { |i| PRIORITY[i.reduce(:&).first] }.sum
