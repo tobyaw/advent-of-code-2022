e48040bd25987ee8382f145aef385bdaa9b6d65e
Toby Atkin-Wright <tobyaw@me.com>
2022-12-04 23:15:25 +0000

    Change the .split into a .scan

--- day_04.rb
+++ day_04.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
 input = File.read('day_04_input.txt')
-            .split(/[\n\-,]/)
+            .scan(/\d+/)
             .map(&:to_i)
             .each_slice(2)
             .map { |i| Array(Range.new(*i)) }
