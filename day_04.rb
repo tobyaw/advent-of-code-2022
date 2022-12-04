f81722fda7454f4b669ac3429dde2879805b94d1
Toby Atkin-Wright <tobyaw@me.com>
2022-12-04 09:54:23 +0000

    Tweaking variable name.

--- day_04.rb
+++ day_04.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 
-numbers = ->(arr) { [*arr.first..arr.last] }
+numbers = ->(i) { [*i.first..i.last] }
 input = File.read('day_04_input.txt').split("\n").map { |i| i.split(',').map { |j| numbers.call(j.split('-').map(&:to_i)) } }
 
 puts input.filter { |i| i.include? i.inject(:&) }.count
