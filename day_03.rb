8a8007c4a1324788326c4ecaf561910c3b7cdddc
Toby Atkin-Wright <tobyaw@me.com>
2022-12-04 22:47:52 +0000

    Tweaking the layout.

--- day_03.rb
+++ day_03.rb
@@ -1,8 +1,12 @@
 #!/usr/bin/env ruby
 
-priority = [*'a'..'z', *'A'..'Z'].zip(1..52).to_h
+priority = [*'a'..'z', *'A'..'Z'].zip(1..52)
+                                 .to_h
 
-input = File.readlines('day_03_input.txt', chomp: true).map(&:chars)
+input = File.readlines('day_03_input.txt', chomp: true)
+            .map(&:chars)
 
 puts input.sum { |i| priority[i.each_slice(i.size / 2).reduce(:&).first] }
-puts input.each_slice(3).sum { |i| priority[i.reduce(:&).first] }
+
+puts input.each_slice(3)
+          .sum { |i| priority[i.reduce(:&).first] }
