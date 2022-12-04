2a1cc5fec45a7141e97b45b50b2c6d5999945977
Toby Atkin-Wright <tobyaw@me.com>
2022-12-04 13:33:42 +0000

    Thinking about better ways to construct the range.

--- day_04.rb
+++ day_04.rb
@@ -1,7 +1,8 @@
 #!/usr/bin/env ruby
 
-numbers = ->(i) { [*i.first..i.last] }
-input = File.read('day_04_input.txt').split("\n").map { |i| i.split(',').map { |j| numbers.call(j.split('-').map(&:to_i)) } }
+input = File.read('day_04_input.txt')
+            .split("\n")
+            .map { |i| i.split(',').map { |j| Range.new(*j.split('-').map(&:to_i)).to_a } }
 
 puts input.filter { |i| i.include? i.reduce(:&) }.count
 puts input.filter { |i| i.reduce(:&).count.positive? }.count
