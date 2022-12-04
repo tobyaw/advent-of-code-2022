9737ae93df5fb5378f9b4b3d080c9cfb15846cc7
Toby Atkin-Wright <tobyaw@me.com>
2022-12-04 09:45:36 +0000

    Function as a lambda — is that less offensive?.

--- day_04.rb
+++ day_04.rb
@@ -1,11 +1,8 @@
 #!/usr/bin/env ruby
 
-def make_array(arr)
-  Range.new(arr[0], arr[1]).to_a
-end
-
+numbers = ->(arr) { [*arr.first..arr.last] }
 input = File.read('day_04_input.txt').split("\n")
-            .map { |i| i.split(',').map { |j| make_array(j.split('-').map(&:to_i)) } }
+            .map { |i| i.split(',').map { |j| numbers.call(j.split('-').map(&:to_i)) } }
 
 puts input.filter { |i| i.include? i.inject(:&) }.count
 puts input.filter { |i| i.inject(:&).count.positive? }.count
