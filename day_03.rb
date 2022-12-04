935a7c3987cae05caf6f7aa97325d3a96ac40f6d
Toby Atkin-Wright <tobyaw@me.com>
2022-12-04 16:39:45 +0000

    Turns out that a .map{}.sum can be expressed as a .sum{}.

--- day_03.rb
+++ day_03.rb
@@ -4,5 +4,5 @@ priority = [*'a'..'z', *'A'..'Z'].zip(1..52).to_h
 
 input = File.read('day_03_input.txt').split("\n").map(&:chars)
 
-puts input.map { |i| priority[i.each_slice(i.size / 2).reduce(:&).first] }.sum
-puts input.each_slice(3).map { |i| priority[i.reduce(:&).first] }.sum
+puts input.sum { |i| priority[i.each_slice(i.size / 2).reduce(:&).first] }
+puts input.each_slice(3).sum { |i| priority[i.reduce(:&).first] }
