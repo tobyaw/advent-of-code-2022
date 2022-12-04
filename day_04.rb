4c034b52db816aa4511d695ff2c4a42570dad0e4
Toby Atkin-Wright <tobyaw@me.com>
2022-12-04 22:43:38 +0000

    Tweaking the layout again.

--- day_04.rb
+++ day_04.rb
@@ -1,8 +1,14 @@
 #!/usr/bin/env ruby
 
-input = File.read('day_04_input.txt').split(/[\n,-]/).map(&:to_i)
-            .each_slice(2).map { |i| [*Range.new(*i)] }
+input = File.read('day_04_input.txt')
+            .split(/[\n,-]/)
+            .map(&:to_i)
+            .each_slice(2)
+            .map { |i| [*Range.new(*i)] }
             .each_slice(2)
 
-puts input.filter { |i| i.include? i.reduce(:&) }.count
-puts input.reject { |i| i.reduce(:&).empty? }.count
+puts input.filter { |i| i.include? i.reduce(:&) }
+          .count
+
+puts input.reject { |i| i.reduce(:&).empty? }
+          .count
