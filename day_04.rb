ced0377bfebfa73a2e9a623ea10f263f5b31bddd
Toby Atkin-Wright <tobyaw@me.com>
2022-12-04 13:54:54 +0000

    Is this more readable? Not sure.

--- day_04.rb
+++ day_04.rb
@@ -1,7 +1,8 @@
 #!/usr/bin/env ruby
 
 input = File.read('day_04_input.txt').split("\n")
-            .map { |i| i.split(',').map { |j| Range.new(*j.split('-').map(&:to_i)).to_a } }
+            .map { |i| i.split(/[,-]/).map(&:to_i) }
+            .map { |i| i.each_slice(2).map { |j| Range.new(*j).to_a } }
 
 puts input.filter { |i| i.include? i.reduce(:&) }.count
 puts input.filter { |i| i.reduce(:&).count.positive? }.count
