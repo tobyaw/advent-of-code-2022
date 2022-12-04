5cb54438e8e7839d27be97b8eac568d3b81ff762
Toby Atkin-Wright <tobyaw@me.com>
2022-12-04 10:02:20 +0000

    Inject… reduce… same thing, but reduce is probably more readable to non-Ruby programmers.

--- day_04.rb
+++ day_04.rb
@@ -3,5 +3,5 @@
 numbers = ->(i) { [*i.first..i.last] }
 input = File.read('day_04_input.txt').split("\n").map { |i| i.split(',').map { |j| numbers.call(j.split('-').map(&:to_i)) } }
 
-puts input.filter { |i| i.include? i.inject(:&) }.count
-puts input.filter { |i| i.inject(:&).count.positive? }.count
+puts input.filter { |i| i.include? i.reduce(:&) }.count
+puts input.filter { |i| i.reduce(:&).count.positive? }.count
