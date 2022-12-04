f42a391890a02419b2861c28fb6075e5b87a333e
Toby Atkin-Wright <tobyaw@me.com>
2022-12-04 16:37:30 +0000

    Removing some unnecessary punctuation.

--- day_03.rb
+++ day_03.rb
@@ -1,6 +1,8 @@
 #!/usr/bin/env ruby
 
-PRIORITY = [*('a'..'z'), *('A'..'Z')].zip((1..52)).to_h
+priority = [*'a'..'z', *'A'..'Z'].zip(1..52).to_h
+
 input = File.read('day_03_input.txt').split("\n").map(&:chars)
-puts input.map { |i| PRIORITY[i.each_slice(i.size / 2).reduce(:&).first] }.sum
-puts input.each_slice(3).map { |i| PRIORITY[i.reduce(:&).first] }.sum
+
+puts input.map { |i| priority[i.each_slice(i.size / 2).reduce(:&).first] }.sum
+puts input.each_slice(3).map { |i| priority[i.reduce(:&).first] }.sum
