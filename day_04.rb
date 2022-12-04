dd348ff71ec1d81acb6f4f72f613922173e0028c
Toby Atkin-Wright <tobyaw@me.com>
2022-12-04 09:50:40 +0000

    Four lines of code.

--- day_04.rb
+++ day_04.rb
@@ -1,8 +1,7 @@
 #!/usr/bin/env ruby
 
 numbers = ->(arr) { [*arr.first..arr.last] }
-input = File.read('day_04_input.txt').split("\n")
-            .map { |i| i.split(',').map { |j| numbers.call(j.split('-').map(&:to_i)) } }
+input = File.read('day_04_input.txt').split("\n").map { |i| i.split(',').map { |j| numbers.call(j.split('-').map(&:to_i)) } }
 
 puts input.filter { |i| i.include? i.inject(:&) }.count
 puts input.filter { |i| i.inject(:&).count.positive? }.count
