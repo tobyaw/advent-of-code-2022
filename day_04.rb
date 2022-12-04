baa1d4834f36c8d9d09015792c630c8df08f7a01
Toby Atkin-Wright <tobyaw@me.com>
2022-12-04 14:02:53 +0000

    Tweaks to format.

--- day_04.rb
+++ day_04.rb
@@ -1,8 +1,7 @@
 #!/usr/bin/env ruby
 
 input = File.read('day_04_input.txt').split("\n")
-            .map { |i| i.split(/[,-]/).map(&:to_i) }
-            .map { |i| i.each_slice(2).map { |j| Range.new(*j).to_a } }
+            .map { |i| i.split(/[,-]/).map(&:to_i).each_slice(2).map { |j| Range.new(*j).to_a } }
 
 puts input.filter { |i| i.include? i.reduce(:&) }.count
 puts input.filter { |i| i.reduce(:&).count.positive? }.count
