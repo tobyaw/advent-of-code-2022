f9c958b33a663918644b5834b413a6d5f0d4d3b1
Toby Atkin-Wright <tobyaw@me.com>
2022-12-04 09:33:06 +0000

    Tweaking range generation.

--- day_04.rb
+++ day_04.rb
@@ -1,12 +1,11 @@
 #!/usr/bin/env ruby
 
 def make_array(arr)
-  arr = arr.split('-').map(&:to_i)
   Range.new(arr[0], arr[1]).to_a
 end
 
 input = File.read('day_04_input.txt').split("\n")
-            .map { |i| i.split(',').map { |j| make_array(j) } }
+            .map { |i| i.split(',').map { |j| make_array(j.split('-').map(&:to_i)) } }
 
 puts input.filter { |i| i.include? i.inject(:&) }.count
 puts input.filter { |i| i.inject(:&).count.positive? }.count
