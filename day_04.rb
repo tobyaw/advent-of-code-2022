7073ead27416313c326b8a0f45dc0508d59aa38e
Toby Atkin-Wright <tobyaw@me.com>
2022-12-04 22:56:05 +0000

    Tweaking regexp escape.

--- day_04.rb
+++ day_04.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
 input = File.read('day_04_input.txt')
-            .split(/[\n,-]/)
+            .split(/[\n\-,]/)
             .map(&:to_i)
             .each_slice(2)
             .map { |i| [*Range.new(*i)] }
