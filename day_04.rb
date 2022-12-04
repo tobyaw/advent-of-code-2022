aeec2920b7935d1cfc554160ceef247d5fd585aa
Toby Atkin-Wright <tobyaw@me.com>
2022-12-04 16:46:02 +0000

    Swapping logic to reduce code size.

--- day_04.rb
+++ day_04.rb
@@ -4,4 +4,4 @@ input = File.read('day_04_input.txt').split("\n")
             .map { |i| i.split(/[,-]/).map(&:to_i).each_slice(2).map { |j| [*Range.new(*j)] } }
 
 puts input.filter { |i| i.include? i.reduce(:&) }.count
-puts input.filter { |i| i.reduce(:&).count.positive? }.count
+puts input.reject { |i| i.reduce(:&).empty? }.count
