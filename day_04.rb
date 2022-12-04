f36d3ad3ff3d15cdfc5373af46d690b9d0c7cf46
Toby Atkin-Wright <tobyaw@me.com>
2022-12-04 13:36:38 +0000

    Tidying layout.

--- day_04.rb
+++ day_04.rb
@@ -1,7 +1,6 @@
 #!/usr/bin/env ruby
 
-input = File.read('day_04_input.txt')
-            .split("\n")
+input = File.read('day_04_input.txt').split("\n")
             .map { |i| i.split(',').map { |j| Range.new(*j.split('-').map(&:to_i)).to_a } }
 
 puts input.filter { |i| i.include? i.reduce(:&) }.count
