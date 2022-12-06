3e35e3f2e9251e69da412ef5de46c89095a5b1a9
Toby Atkin-Wright <tobyaw@me.com>
2022-12-06 11:14:32 +0000

    Remove unnecessary .first

--- day_06.rb
+++ day_06.rb
@@ -4,6 +4,6 @@ input = File.read('day_06_input.txt').chars
 
 [4, 14].each do |i|
   puts input.each_cons(i).with_index
-            .find { |j| j.first.uniq.count.eql? i }
+            .find { |j, _| j.uniq.count.eql? i }
             .last + i
 end
