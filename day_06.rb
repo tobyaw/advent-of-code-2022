ce40b726fe82412f2d4a3db73ee27c7b0a260c54
Toby Atkin-Wright <tobyaw@me.com>
2022-12-06 10:53:41 +0000

    Simplifying code.

--- day_06.rb
+++ day_06.rb
@@ -1,13 +1,8 @@
 #!/usr/bin/env ruby
 
-def process(input, length)
-  input.each_cons(length)
-       .map.with_index { |i, j| [i, j] }
-       .select { |i| i.first.uniq.count.eql? length }
-       .map(&:last)
-       .first + length
-end
-
 input = File.read('day_06_input.txt').chars
-puts process(input, 4)
-puts process(input, 14)
+[4, 14].each do |length|
+  puts input.each_cons(length).with_index
+            .find { |i| i.first.uniq.count.eql? length }
+            .last + length
+end
