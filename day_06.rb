698be6c476893c5168414eae2907cb3053916bcb
Toby Atkin-Wright <tobyaw@me.com>
2022-12-06 10:56:32 +0000

    Tweaking formatting.

--- day_06.rb
+++ day_06.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 
 input = File.read('day_06_input.txt').chars
+
 [4, 14].each do |length|
   puts input.each_cons(length).with_index
             .find { |i| i.first.uniq.count.eql? length }
