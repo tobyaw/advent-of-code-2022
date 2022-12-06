66452b7ad618aa93be7bf31b548200d850244044
Toby Atkin-Wright <tobyaw@me.com>
2022-12-06 11:38:43 +0000

    Changing .each_cons().with_index.find into .each_cons().find_index

--- day_06.rb
+++ day_06.rb
@@ -2,4 +2,4 @@
 
 input = File.read('day_06_input.txt').chars
 
-puts [4, 14].map { |i| i + input.each_cons(i).with_index.find { |j, _| j.eql? j.uniq }.last }
+puts [4, 14].map { |i| i + input.each_cons(i).find_index { |j| j.eql? j.uniq } }
