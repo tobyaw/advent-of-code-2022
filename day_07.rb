35595be3a1909231c53fdf617ebcada2bee2e626
Toby Atkin-Wright <tobyaw@me.com>
2022-12-07 10:47:59 +0000

    Tidying layout and adding a guard on the file parsing.

--- day_07.rb
+++ day_07.rb
@@ -1,18 +1,15 @@
 #!/usr/bin/env ruby
 
-input = File.readlines('day_07_input.txt', chomp: true)
-            .map(&:split)
-
 folders = Hash.new(0)
 current_folder = []
 
-input.each do |line|
+File.readlines('day_07_input.txt', chomp: true).map(&:split).each do |line|
   case line
   in ['$', 'cd', '..']
     current_folder.pop
   in ['$', 'cd', folder]
     current_folder.push folder
-  in [size, file]
+  in [size, file] if size.match?(/^\d+$/)
     path = []
     current_folder.each do |folder|
       path.push folder
