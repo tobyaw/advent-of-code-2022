c983864d49d3376b23e83522dc5db8e8e7d22aaa
Toby Atkin-Wright <tobyaw@me.com>
2022-12-07 12:18:49 +0000

    Renaming folders to folder_sizes to make things a bit clearer.

--- day_07.rb
+++ day_07.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 
-folders = Hash.new(0)
+folder_sizes = Hash.new(0)
 
 File.readlines('day_07_input.txt', chomp: true).map(&:split).each_with_object([]) do |line, stack|
   case line
@@ -9,10 +9,10 @@ File.readlines('day_07_input.txt', chomp: true).map(&:split).each_with_object([]
   in ['$', 'cd', folder]
     stack.push [stack.last, folder].compact.join(' ')
   in [size, file] if size.match?(/^\d+$/)
-    stack.each { |i| folders[i] += size.to_i }
+    stack.each { |i| folder_sizes[i] += size.to_i }
   else
   end
 end
 
-puts folders.values.reject { |i| i > 100_000 }.sum
-puts folders.values.reject { |i| i < folders['/'] - 40_000_000 }.min
+puts folder_sizes.values.reject { |i| i > 100_000 }.sum
+puts folder_sizes.values.reject { |i| i < folder_sizes['/'] - 40_000_000 }.min
