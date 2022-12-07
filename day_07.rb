6beafb1fd3883d096b960bc8f6e630c8199ef17b
Toby Atkin-Wright <tobyaw@me.com>
2022-12-07 11:27:38 +0000

    Starting to think about a different way of expressing the working data.

--- day_07.rb
+++ day_07.rb
@@ -2,19 +2,18 @@
 
 folders = Hash.new(0)
 current_folder = []
+current_folder_stack = []
 
 File.readlines('day_07_input.txt', chomp: true).map(&:split).each do |line|
   case line
   in ['$', 'cd', '..']
     current_folder.pop
+    current_folder_stack.pop
   in ['$', 'cd', folder]
     current_folder.push folder
+    current_folder_stack.push current_folder.join(' ')
   in [size, file] if size.match?(/^\d+$/)
-    path = []
-    current_folder.each do |folder|
-      path.push folder
-      folders[path.join '/'] += size.to_i
-    end
+    current_folder_stack.each { |i| folders[i] += size.to_i }
   else
   end
 end
