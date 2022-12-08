5dafd04f0533337e8538f27f01dfec3d524ec34d
Toby Atkin-Wright <tobyaw@me.com>
2022-12-08 17:30:24 +0000

    Breaking out the row processor into a method.

--- day_08.rb
+++ day_08.rb
@@ -1,19 +1,17 @@
 #!/usr/bin/env ruby
 
+def process(row)
+  row.reduce([-1]) do |memo, cell|
+    cell[:visible] = true if cell[:height] > memo.max
+    cell[:scores] << ((memo.index { |i| i >= cell[:height] } || (memo.size - 2)) + 1)
+    [cell[:height]] + memo
+  end
+end
+
 input = File.readlines('day_08_input.txt', chomp: true)
             .map { |i| i.chars.map { |j| { height: j.to_i, visible: false, scores: [] } } }
 
-[input, input.transpose].each do |grid|
-  grid.each do |grid_row|
-    [grid_row, grid_row.reverse].each do |row|
-      row.reduce([-1]) do |memo, cell|
-        cell[:visible] = true if cell[:height] > memo.max
-        cell[:scores] << ((memo.index { |i| i >= cell[:height] } || (memo.size - 2)) + 1)
-        [cell[:height]] + memo
-      end
-    end
-  end
-end
+[input, input.transpose].each { |i| i.each { |j| [j, j.reverse].each { |k| process k } } }
 
 puts input.flatten.select { |i| i[:visible].eql? true }.count
 puts input.flatten.map { |i| i[:scores].reduce(:*) }.max
