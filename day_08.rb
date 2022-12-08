6876e23dee2c70e46b7e8f49b654d54c512a2db3
Toby Atkin-Wright <tobyaw@me.com>
2022-12-08 12:54:25 +0000

    Using the reduce accumulator for the height calculation.

--- day_08.rb
+++ day_08.rb
@@ -6,14 +6,8 @@ input = File.readlines('day_08_input.txt', chomp: true)
 [input, input.transpose].each do |grid|
   grid.each do |grid_row|
     [grid_row, grid_row.reverse].each do |row|
-      highest = -1
-
       row.reduce([]) do |so_far, cell|
-        if cell[:height] > highest
-          highest = cell[:height]
-          cell[:visible] = true
-        end
-
+        cell[:visible] = true if cell[:height] > (so_far.max || -1)
         cell[:scores].push((so_far.reverse.index { |i| i >= cell[:height] } || (so_far.size - 1)) + 1)
         so_far + [cell[:height]]
       end
