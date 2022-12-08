4c52a332b636c0ffdb56cc527fe4a60df1b695bb
Toby Atkin-Wright <tobyaw@me.com>
2022-12-08 12:23:26 +0000

    A scruffy first draft.

---
+++ day_08.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/env ruby
+
+input = File.readlines('day_08_input.txt', chomp: true)
+            .map { |i| i.chars.map { |j| { height: j.to_i, visible: false, score: [] } } }
+
+[input, input.transpose].each do |i|
+  i.each do |j|
+    [j, j.reverse].each do |k|
+      highest = -1
+      last = 0
+      heights_so_far = []
+
+      k.each do |l|
+        if l[:height] > highest
+          highest = l[:height]
+          l[:visible] = true
+        end
+
+        visible = 0
+        heights_so_far.reverse.each do |item|
+          visible += 1
+          break if item >= l[:height]
+        end
+
+        l[:score].push visible
+        heights_so_far.push l[:height]
+        last = l[:height]
+      end
+    end
+  end
+end
+
+puts input.flatten.select { |i| i[:visible].eql? true }.count
+puts input.flatten.map { |i| i[:score].reduce(:*) }.max
