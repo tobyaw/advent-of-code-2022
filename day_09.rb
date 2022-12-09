078c0e4a7ea4c45a6225dcabea29020ac5f6e45a
Toby Atkin-Wright <tobyaw@me.com>
2022-12-09 15:03:29 +0000

    Treating coordinates as arrays rather than as hashes.

--- day_09.rb
+++ day_09.rb
@@ -5,32 +5,32 @@ MOVES = { 'D' => [0, -1], 'U' => [0, 1], 'L' => [-1, 0], 'R' => [1, 0] }
 input = File.readlines('day_09_input.txt', chomp: true).map(&:split)
 
 [2, 10].each do |length|
-  coords = (1..length).map { { x: 0, y: 0 } }
-  positions = [coords.first.values]
+  coords = (1..length).map { [0, 0] }
+  positions = [coords.first.join('x')]
 
   input.each do |direction, distance|
     distance.to_i.times do
-      coords.first[:x] += MOVES[direction][0]
-      coords.first[:y] += MOVES[direction][1]
+      coords.first[0] += MOVES[direction][0]
+      coords.first[1] += MOVES[direction][1]
 
       coords.each_cons(2) do |i, j|
-        if (i[:x] - j[:x]).abs > 1 && (i[:y] - j[:y]).abs > 1
-          j[:x] = i[:x] + (j[:x] > i[:x] ? 1 : -1)
-          j[:y] = i[:y] + (j[:y] > i[:y] ? 1 : -1)
-        elsif (i[:y].eql? j[:y]) && (i[:x] - j[:x]).abs > 1
-          j[:x] = i[:x] + (j[:x] > i[:x] ? 1 : -1)
-        elsif (i[:x].eql? j[:x]) && (i[:y] - j[:y]).abs > 1
-          j[:y] = i[:y] + (j[:y] > i[:y] ? 1 : -1)
-        elsif (i[:x] - j[:x]).abs > 1
-          j[:x] = i[:x] + (j[:x] > i[:x] ? 1 : -1)
-          j[:y] = i[:y]
-        elsif (i[:y] - j[:y]).abs > 1
-          j[:x] = i[:x]
-          j[:y] = i[:y] + (j[:y] > i[:y] ? 1 : -1)
+        if (i[0] - j[0]).abs > 1 && (i[1] - j[1]).abs > 1
+          j[0] = i[0] + (j[0] > i[0] ? 1 : -1)
+          j[1] = i[1] + (j[1] > i[1] ? 1 : -1)
+        elsif (i[1].eql? j[1]) && (i[0] - j[0]).abs > 1
+          j[0] = i[0] + (j[0] > i[0] ? 1 : -1)
+        elsif (i[0].eql? j[0]) && (i[1] - j[1]).abs > 1
+          j[1] = i[1] + (j[1] > i[1] ? 1 : -1)
+        elsif (i[0] - j[0]).abs > 1
+          j[0] = i[0] + (j[0] > i[0] ? 1 : -1)
+          j[1] = i[1]
+        elsif (i[1] - j[1]).abs > 1
+          j[0] = i[0]
+          j[1] = i[1] + (j[1] > i[1] ? 1 : -1)
         end
       end
 
-      positions.push coords.last.values
+      positions.push coords.last.join('x')
     end
   end
 
