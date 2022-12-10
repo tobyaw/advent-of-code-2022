b6393442570ec2bedfd249c12019c2c1681e3e77
Toby Atkin-Wright <tobyaw@me.com>
2022-12-10 16:36:32 +0000

    Only splitting when we need to.

--- day_10.rb
+++ day_10.rb
@@ -1,10 +1,9 @@
 #!/usr/bin/env ruby
 
 h = File.readlines('day_10_input.txt', chomp: true)
-        .map(&:split)
         .each_with_object([1]) do |i, a|
   a.push a.last
-  a.push(a.last + i[1].to_i) if i[0].eql? 'addx'
+  a.push(a.last + i.split.last.to_i) if i.start_with? 'addx'
 end
 
 puts 20.step(by: 40, to: 220)
