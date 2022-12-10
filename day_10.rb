b4e080c79db7fe0228e1944c2c21d66d6d2c5b1d
Toby Atkin-Wright <tobyaw@me.com>
2022-12-10 14:42:15 +0000

    Using the final element of the history as the register storage.

--- day_10.rb
+++ day_10.rb
@@ -1,15 +1,10 @@
 #!/usr/bin/env ruby
 
-history = []
+history = [1]
 
-File.readlines('day_10_input.txt', chomp: true).map(&:split)
-    .each.with_object({ x: 1 }) do |line, register|
-  history.push register[:x]
-
-  if line[0].eql? 'addx'
-    history.push register[:x]
-    register[:x] += line[1].to_i
-  end
+File.readlines('day_10_input.txt', chomp: true).map(&:split).each do |line|
+  history.push history.last
+  history.push(history.last + line[1].to_i) if line[0].eql? 'addx'
 end
 
 puts 20.step(by: 40, to: 220).sum { |i| i * history[i - 1] }
