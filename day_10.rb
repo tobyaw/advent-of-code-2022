68565acb0653d5ed45cab2b91947951ee27489e0
Toby Atkin-Wright <tobyaw@me.com>
2022-12-10 14:55:21 +0000

    Broke out the sprite range as a .upto().map {}

--- day_10.rb
+++ day_10.rb
@@ -1,14 +1,14 @@
 #!/usr/bin/env ruby
 
-history = [1]
+h = [1]
 
-File.readlines('day_10_input.txt', chomp: true).map(&:split).each do |line|
-  history.push history.last
-  history.push(history.last + line[1].to_i) if line[0].eql? 'addx'
+File.readlines('day_10_input.txt', chomp: true).map(&:split).each do |i|
+  h.push h.last
+  h.push(h.last + i[1].to_i) if i[0].eql? 'addx'
 end
 
-puts 20.step(by: 40, to: 220).sum { |i| i * history[i - 1] }
+puts 20.step(by: 40, to: 220).sum { |i| i * h[i - 1] }
 
 240.times
-   .map { |i| (history[i] - 1).upto(history[i] + 1).include?(i % 40) ? '#' : '.' }
+   .map { |i| -1.upto(1).map { |j| j + h[i] }.include?(i % 40) ? '#' : '.' }
    .each_slice(40) { |i| puts i.join }
