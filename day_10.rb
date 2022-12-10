3beaa2db93115daefba06701a967f3403a73654a
Toby Atkin-Wright <tobyaw@me.com>
2022-12-10 19:10:02 +0000

    Simpler calculation — .abs and comparison rather than array .include?

--- day_10.rb
+++ day_10.rb
@@ -2,13 +2,13 @@
 
 h = File.readlines('day_10_input.txt', chomp: true)
         .each_with_object([1]) do |i, a|
-  a.push a.last
-  a.push(a.last + i.split.last.to_i) if i.start_with? 'addx'
+  a << a.last
+  a << (a.last + i.split.last.to_i) if i.start_with? 'addx'
 end
 
 puts 20.step(by: 40, to: 220)
        .sum { |i| i * h[i - 1] }
 
 240.times
-   .map { |i| -1.upto(1).include?((i % 40) - h[i]) ? '#' : '.' }
+   .map { |i| (h[i] - (i % 40)).abs < 2 ? '#' : '.' }
    .each_slice(40) { |i| puts i.join }
