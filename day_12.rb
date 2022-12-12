0c2996056721795506bdb433226d984c4300168c
Toby Atkin-Wright <tobyaw@me.com>
2022-12-12 23:15:58 +0000

    Minor tweaks to names.

--- day_12.rb
+++ day_12.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 
-def find_next_steps(grid, heads, to, polarity, depth = 1)
+def find_next_steps(grid, heads, target, polarity, depth = 1)
   return if heads.empty?
 
   next_heads = []
@@ -14,19 +14,19 @@ def find_next_steps(grid, heads, to, polarity, depth = 1)
             .reject { |x, y| grid[y][x][:seen].eql? true }
             .filter { |x, y| polarity * (grid[y][x][:h] - grid[yh][xh][:h]) < 2 }
 
-    raise depth.to_s if steps.map { |x, y| grid[y][x][:char] }.include? to
+    raise depth.to_s if steps.map { |x, y| grid[y][x][:char] }.include? target
 
     next_heads += steps
   end
 
-  find_next_steps(grid, next_heads.uniq, to, polarity, depth + 1)
+  find_next_steps(grid, next_heads.uniq, target, polarity, depth + 1)
 end
 
 input = File.readlines('day_12_input.txt', chomp: true)
 
 [
-  { from: 'S', to: 'E', polarity: 1 },
-  { from: 'E', to: 'a', polarity: -1 }
+  { from: 'S', target: 'E', polarity: 1 },
+  { from: 'E', target: 'a', polarity: -1 }
 ].each do |part|
   y = input.find_index { |i| i.include? part[:from] }
   x = input[y].index(part[:from])
@@ -35,7 +35,7 @@ input = File.readlines('day_12_input.txt', chomp: true)
     i.chars.map { |j| { char: j, h: j.tr('SE', 'az').ord, seen: false } }
   end
 
-  find_next_steps(grid, [[x, y]], part[:to], part[:polarity])
+  find_next_steps(grid, [[x, y]], part[:target], part[:polarity])
 rescue StandardError => e
   puts e.message
 end
