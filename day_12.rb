90e43eb304875e369dc4c19835f9bfb2a9d10db2
Toby Atkin-Wright <tobyaw@me.com>
2022-12-12 22:17:03 +0000

    A bit more tidying. Shorter lines.

--- day_12.rb
+++ day_12.rb
@@ -1,8 +1,5 @@
 #!/usr/bin/env ruby
 
-filename = 'day_12_input.txt'
-input = File.readlines(filename, chomp: true)
-
 def find_char(grid, item)
   y = grid.find_index { |i| i.include? item }
   x = grid[y].index(item)
@@ -15,13 +12,12 @@ def find_target(grid, heads, target, depth = 1)
 
   heads.each do |xh, yh|
     grid[yh][xh][:seen] = true
-    height = grid[yh][xh][:height]
 
     directions = [[xh, yh + 1], [xh, yh - 1], [xh + 1, yh], [xh - 1, yh]]
                  .reject { |x, y| x.negative? || y.negative? }
-                 .select { |x, y| (x < grid.first.size) && (y < grid.size) }
+                 .filter { |x, y| (x < grid.first.size) && (y < grid.size) }
                  .reject { |x, y| grid[y][x][:seen].eql? true }
-                 .select { |x, y| grid[y][x][:height] - height < 2 }
+                 .filter { |x, y| grid[y][x][:height] - grid[yh][xh][:height] < 2 }
 
     raise depth.to_s if directions.map { |x, y| grid[y][x][:char] }.include? target
 
@@ -33,11 +29,20 @@ def find_target(grid, heads, target, depth = 1)
   find_target(grid, outputs.uniq, target, depth + 1)
 end
 
+input = File.readlines('day_12_input.txt', chomp: true)
+
 [
   { start: 'S', target: 'E', tr: 'az', atoz: 'a-z' },
   { start: 'E', target: 'z', tr: 'za', atoz: ('a'..'z').to_a.join.reverse }
 ].each do |part|
-  grid = input.map { |i| i.tr('a-z', part[:atoz]).chars.map { |j| { char: j, height: j.tr('SE', part[:tr]).ord, seen: false } } }
+  grid = input.map do |row|
+    row.tr('a-z', part[:atoz])
+       .chars
+       .map do |char|
+      { char:, height: char.tr('SE', part[:tr]).ord, seen: false }
+    end
+  end
+
   find_target(grid, [find_char(input, part[:start])], part[:target])
 rescue StandardError => e
   puts e.message
