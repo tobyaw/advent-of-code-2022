878448c1b8b94aa638bbda7852469d2025efef2c
Toby Atkin-Wright <tobyaw@me.com>
2022-12-13 15:12:11 +0000

    Tidying up comparison function.

--- day_13.rb
+++ day_13.rb
@@ -7,35 +7,28 @@ def compare(left, right)
   return true if left.empty?
   return false if right.empty?
 
-  lf = left.first
-  rf = right.first
+  heads = [left.first, right.first]
 
-  case [lf, rf]
+  case heads
   in [Integer, Integer]
-    return true if lf < rf
-    return false if lf > rf
+    return true if heads.reduce(:<)
+    return false if heads.reduce(:>)
   else
-    lf = [lf] if lf.instance_of?(Integer)
-    rf = [rf] if rf.instance_of?(Integer)
-    xyz = compare(lf, rf)
-    return xyz if [true, false].include? xyz
+    arr_heads = heads.map { |i| i.instance_of?(Integer) ? [i] : i }
+    arr_comp = compare(arr_heads[0], arr_heads[1])
+    return arr_comp if [true, false].include? arr_comp
   end
 
   compare(left.slice(1..), right.slice(1..))
 end
 
-input = File.read('day_13_input.txt')
-            .split("\n\n")
+input = File.read('day_13_input.txt').split("\n\n")
             .map { |i| i.split("\n").map { |j| JSON.parse(j) } }
 
 dividers = [2, 6].map { |i| [[i]] }
 
-packets = input.reduce([]) { |acc, (i, j)| acc << i << j }
-               .concat(dividers)
+packets = input.reduce([]) { |acc, (i, j)| acc << i << j }.concat(dividers)
                .sort_by { |i| i.to_s.gsub(/\[\]/, '0').gsub(/[\[\]]/, '').split(', ').map(&:to_i) }
 
-puts input.map.with_index { |(l, r), i| compare(l, r).eql?(true) ? i + 1 : 0 }
-          .sum
-
-puts dividers.map { |i| packets.index(i) + 1 }
-             .reduce(:*)
+puts input.map.with_index { |(l, r), i| compare(l, r) ? i + 1 : 0 }.sum
+puts dividers.map { |i| packets.index(i) + 1 }.reduce(:*)
