ef3fc72217c58a084576230bcc6024b77a86fef0
Toby Atkin-Wright <tobyaw@me.com>
2022-12-14 15:02:54 +0000

    Compacting the code.

--- day_14.rb
+++ day_14.rb
@@ -1,7 +1,6 @@
 #!/usr/bin/env ruby
 
-input = File.readlines('day_14_input.txt', chomp: true)
-            .map { |i| i.split(' -> ') }
+input = File.readlines('day_14_input.txt', chomp: true).map { |i| i.split(' -> ') }
 
 [false, true].each do |floor|
   x_min, x_max = input.flatten.map(&:to_i).minmax
@@ -14,38 +13,33 @@ input = File.readlines('day_14_input.txt', chomp: true)
     input << [x_min, x_max].map { |i| "#{i},#{y_max}" }
   end
 
-  grid = x_min.upto(x_max)
-              .to_h { |i| [i, ['.'] * (y_max + 1)] }
+  grid = (x_min..x_max).to_h { |i| [i, ['.'] * (y_max + 1)] }
 
   input.each do |i|
-    i.map { |j| j.split(',').map(&:to_i) }
-     .each_cons(2) do |j, k|
-      xs = [j, k].map(&:first).sort
-      ys = [j, k].map(&:last).sort
-
-      (xs[0]..xs[1]).each do |x|
-        (ys[0]..ys[1]).each do |y|
+    i.map { |j| j.split(',').map(&:to_i) }.each_cons(2) do |j, k|
+      Range.new(*[j, k].map(&:first).sort).each do |x|
+        Range.new(*[j, k].map(&:last).sort).each do |y|
           grid[x][y] = '#'
         end
       end
     end
   end
 
-  (0..).each do |i|
-    x = 500
-    y = 0
+  (0..).each_with_object([500, 0]) do |i, (x, y)|
     raise i.to_s unless grid[x][y].eql? '.'
 
     loop do
-      moves = [x, x - 1, x + 1].map { |j| [j, y + 1] }
-      moves.each do |j, k|
-        raise i.to_s unless (x_min..x_max).include?(j) && (0..y_max).include?(k)
+      moves = [0, -1, 1]
+              .map { |j| [x + j, y + 1] }
+              .filter do |j, k|
+        raise i.to_s unless (x_min..x_max).include?(j)
+        raise i.to_s unless (0..y_max).include?(k)
+
+        grid[j][k].eql? '.'
       end
+      break if moves.empty?
 
-      move = moves.filter { |j, k| grid[j][k].eql?('.') }.first
-      break if move.nil?
-
-      x, y = move
+      x, y = moves.first
     end
 
     grid[x][y] = 'o'
