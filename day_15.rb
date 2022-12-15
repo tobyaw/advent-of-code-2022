1b648b371d6675f1cabacdd0c6fc32f1fa272ed0
Toby Atkin-Wright <tobyaw@me.com>
2022-12-15 23:57:02 +0000

    A bit tidier, but still quite slow.

--- day_15.rb
+++ day_15.rb
@@ -1,19 +1,15 @@
 #!/usr/bin/env ruby
 
-example1, part1 = [
-  { filename: 'day_15_example.txt', y: 10 },
-  { filename: 'day_15_input.txt', y: 2_000_000 }
-].map do |run|
+def part1
   ranges = []
   exclude = []
 
-  File.readlines(run[:filename], chomp: true)
+  File.readlines('day_15_input.txt', chomp: true)
       .map { |i| i.scan(/-?\d+/).map(&:to_i) }
       .map do |sx, sy, bx, by|
-    exclude << bx if by.eql? run[:y]
+    exclude << bx if by.eql? 2_000_000
 
-    distance = (sx - bx).abs + (sy - by).abs
-    remainder = distance - (sy - run[:y]).abs
+    remainder = (sx - bx).abs + (sy - by).abs - (sy - 2_000_000).abs
     next unless remainder.positive?
 
     ranges << ((sx - remainder)..(sx + remainder))
@@ -21,48 +17,42 @@ example1, part1 = [
 
   ranges = ranges.sort_by { |i| [i.first, i.last] }
                  .each_with_object([]) do |item, sum|
-    if sum.empty? || sum.last.last < (item.first - 1)
+    if sum.empty? || sum[-1].last < (item.first - 1)
       sum << item
     else
-      sum[-1] = sum.last.first..([sum.last.last, item.last].max)
+      sum[-1] = sum[-1].first..([sum[-1].last, item.last].max)
     end
   end
 
   exclude = exclude.uniq.reject { |i| ranges.index { |j| j.include? i }.nil? }
-
   ranges.reduce(0) { |sum, i| sum + i.size } - exclude.count
 end
 
-puts "Example 1 #{example1}"
-puts "Part 1 #{part1}"
-
-raise unless example1.eql? 26
-raise unless part1.eql? 6_425_133
-
-example2, part2 = [
-  { filename: 'day_15_example.txt', limit: 20 },
-  { filename: 'day_15_input.txt', limit: 4_000_000 }
-].map do |run|
-  input = File.readlines(run[:filename], chomp: true)
+def part2
+  input = File.readlines('day_15_input.txt', chomp: true)
               .map { |i| i.scan(/-?\d+/).map(&:to_i) }
+              .map do |sx, sy, bx, by|
+    distance = (sx - bx).abs + (sy - by).abs
+    [sx, sy, bx, by, distance]
+  end
 
   x = nil
-  y = 0.upto(run[:limit]).find_index do |row|
-    puts row if (row % 100_000).eql? 0
+  y = 0.upto(4_000_000).find_index do |row|
     ranges = []
 
-    input.map do |sx, sy, bx, by|
-      distance = (sx - bx).abs + (sy - by).abs
+    input.map do |sx, sy, _bx, _by, distance|
       remainder = distance - (sy - row).abs
       next unless remainder.positive?
 
       r1 = sx - remainder
-      r2 = sx + remainder
-      next if r1 > run[:limit]
-      next if r2.negative?
+      next if r1 > 4_000_000
 
       r1 = 0 if r1.negative?
-      r2 = run[:limit] if r2 > run[:limit]
+
+      r2 = sx + remainder
+      next if r2.negative?
+
+      r2 = 4_000_000 if r2 > 4_000_000
 
       ranges << (r1..r2)
     end
@@ -76,24 +66,15 @@ example2, part2 = [
       end
     end
 
-    response = false
-
-    if ranges.count > 1
-      x = ranges.first.last + 1
-      response = true
-    end
-
-    response
+    x = ranges.first.last + 1
+    true if ranges.count > 1
   end
 
-  puts "row #{x} column #{y}"
-
-  puts "x #{x} y #{y}"
   (x * 4_000_000) + y
 end
 
-puts "Example 2 #{example2}"
-puts "Part 2 #{part2}"
+puts part1_answer = part1
+raise unless part1_answer.eql? 6_425_133
 
-raise unless example2.eql? 56_000_011
-raise unless part2.eql? 10_996_191_429_555
+puts part2_answer = part2
+raise unless part2_answer.eql? 10_996_191_429_555
