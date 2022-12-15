0b093afe108e1301c234b642023d674864e20d21
Toby Atkin-Wright <tobyaw@me.com>
2022-12-15 17:44:19 +0000

    Tidied up, and added a little test.

--- day_15.rb
+++ day_15.rb
@@ -1,10 +1,11 @@
 #!/usr/bin/env ruby
 
-[
+example1, part1 = [
   { filename: 'day_15_example.txt', y: 10 },
   { filename: 'day_15_input.txt', y: 2_000_000 }
-].each do |run|
-  input = File.readlines(run[:filename], chomp: true).map { |i| i.scan(/-?\d+/).map(&:to_i) }
+].map do |run|
+  input = File.readlines(run[:filename], chomp: true)
+              .map { |i| i.scan(/-?\d+/).map(&:to_i) }
 
   on_line = []
   exclude = []
@@ -19,5 +20,11 @@
     on_line << ((sx - remainder)..(sx + remainder))
   end
 
-  puts on_line.map(&:to_a).flatten.uniq.count - exclude.uniq.count
+  on_line.map(&:to_a).flatten.uniq.count - exclude.uniq.count
 end
+
+puts "Example 1 #{example1}"
+puts "Part 1 #{part1}"
+
+raise unless example1.eql? 26
+raise unless part1.eql? 6_425_133
